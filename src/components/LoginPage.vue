79411b2e4cadec68463ebb79a57a86f3c58adf9d
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-03-06 12:26:34 -0300

    feat: criar componente de login

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container mt-5">
+      <h1>Olá</h1>
+    </div>
+</template>
+  
+  <script setup>
+  /* eslint-disable vue/multi-word-component-names */
+  
+ 
+  
+  </script>
+  
+  <style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  </style>
+  
