63105b9b0cb0f963e2068976c69303444b84c8cd
Luiz Fernando <luisferndantas@outlook.com>
2025-03-29 22:30:07 -0300

    feat(courses): :sparkles: Create form to add new courses to the plataform
    new courses

---
+++ src/components/pages/CreateCourse.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="create-course container d-flex justify-content-center align-items-center pb-5">
+      <div class="card bg-purple text-white w-50">
+        <div class="card-body">
+          <h5 class="card-title">Criar Novo Curso</h5>
+          <FormCreateCourse />
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import FormCreateCourse from '../organisms/FormCreateCourse.vue';
+  
+  export default {
+    name: 'CreateCourse',
+    components: {
+      FormCreateCourse,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .bg-purple {
+    background-color: #B288C0 !important; /* Cor roxa do Bootstrap 5 */
+  }
+  </style>
+  
