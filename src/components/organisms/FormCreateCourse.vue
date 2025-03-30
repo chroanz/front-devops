f813a595c86a45f8c599a243c61c9b9a2ffec881
Luiz Fernando <luisferndantas@outlook.com>
2025-03-29 23:26:57 -0300

    fix: :art: desing form
    d-flex in form

--- src/components/organisms/FormCreateCourse.vue
+++ src/components/organisms/FormCreateCourse.vue
@@ -1,57 +1,56 @@
 <template>
-  <div class="container">
-    <form @submit.prevent="submitForm">
-      <div class="mb-3">
-        <label for="courseName" class="form-label">Nome do Curso</label>
-        <input type="text" class="form-control" id="courseName" v-model="course.name" required>
-      </div>
-      <div class="mb-3">
-        <label for="courseDescription" class="form-label">Descrição</label>
-        <textarea class="form-control" id="courseDescription" v-model="course.description" required></textarea>
-      </div>
-      <div class="row col-12 mb-3">
-        <div class="col-sm-6">
-            <label for="courseImage" class="form-label">Imagem</label>
-            <input type="file" class="form-control" id="courseImage" @change="handleImageUpload">
-        </div>
-        <div class="col-sm-6">
-            <label for="type" class="form-label">Tipo do curso</label>
-            <select name="type" v-model="course.type" class="form-control" id="type">
-            <option value="0" >Selecione o alvo para esse curso</option>
-            <option value="1">Deficiência visual</option>
-            <option value="2">Deficiência auditiva</option>
-            <option value="3">Surdocegueira</option>
-        </select>
-        </div>
-      </div>
-      <div class="d-flex align-items-end">
-        <button type="submit" class="btn btn-primary m-1">Criar Curso</button>
-        <a href="" class="btn btn-secondary m-1">Cancelar</a>
-      </div>
-    </form>
-  </div>
+    <div class="container">
+        <form @submit.prevent="submitForm" class="d-flex flex-column ">
+            <div class="mb-3">
+                <label for="courseName" class="form-label">Nome do Curso</label>
+                <input type="text" class="form-control" id="courseName" v-model="course.name" required>
+            </div>
+            <div class="mb-3">
+                <label for="courseDescription" class="form-label">Descrição</label>
+                <textarea class="form-control" id="courseDescription" v-model="course.description" required></textarea>
+            </div>
+            <div class="col-12 mb-3 flex-row d-flex justify-content-between">
+                <div class="col-sm-6 w-50">
+                    <label for="courseImage" class="form-label">Imagem</label>
+                    <input type="file" class="form-control" id="courseImage" @change="handleImageUpload">
+                </div>
+                <div class="col-sm-6 w-50 mx-1">
+                    <label for="type" class="form-label">Tipo do curso</label>
+                    <select name="type" v-model="course.type" class="form-control" id="type" required placeholder="Selecione o tipo de curso">
+                        <option value="1">Deficiência visual</option>
+                        <option value="2">Deficiência auditiva</option>
+                        <option value="3">Surdocegueira</option>
+                    </select>
+                </div>
+            </div>
+            <div class="d-flex align-items-end">
+                <button type="submit" class="btn btn-primary m-1">Criar Curso</button>
+                <a href="" class="btn btn-secondary m-1">Cancelar</a>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'FormCreateCourse',
-  data() {
-    return {
-      course: {
-        name: '',
-        type: '',
-        description: '',
-        image: null,
-      },
-    };
-  },
-  methods: {
-    handleImageUpload(event) {
-      this.course.image = event.target.files[0];
+    name: 'FormCreateCourse',
+    data() {
+        return {
+            course: {
+                name: '',
+                type: '',
+                description: '',
+                image: null,
+            },
+        };
     },
-    submitForm() {
-      console.log('Dados do curso:', this.course);
-      // Aqui você pode adicionar a lógica para enviar os dados do curso para o backend
+    methods: {
+        handleImageUpload(event) {
+            this.course.image = event.target.files[0];
+        },
+        submitForm() {
+            console.log('Dados do curso:', this.course);
+            // Aqui você pode adicionar a lógica para enviar os dados do curso para o backend
         },
     },
 } 
