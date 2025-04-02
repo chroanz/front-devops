921515ea372fc66856315be567bda70278d4426b
Thormes Filgueira Leite Pereira <thormes@gmail.com>
2025-04-02 15:17:42 -0300

    refactor: :art: Alterar Progress bar para pegar por padrão a cor de ação da aplicação
    Alteração do componente ProgressBar para usar por padrão a cor de ação da aplicação. Adicionar propriedade para que possa ser informada a cor da barra de progresso

--- src/components/molecules/ProgressBar.vue
+++ src/components/molecules/ProgressBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="progress-bar">
-    <div class="progress-bar-fill" :style="{ width: progress + '%' }"></div>
+    <div class="progress-bar-fill" :style="{ width: progress + '%', 'background-color': color }"></div>
   </div>
 </template>
 
@@ -14,6 +14,11 @@ export default {
       validator(value) {
         return value >= 0 && value <= 100;
       }
+    },
+    color: {
+      type: String,
+      required: false,
+      default: getComputedStyle(document.documentElement)?.getPropertyValue('--color-action')?.trim() ?? '#4caf50'
     }
   }
 };
@@ -21,7 +26,7 @@ export default {
 
 <style scoped>
 .progress-bar {
-  width: 80%;
+  width: 100%;
   height: 20px;
   background-color: #e0e0e0;
   border-radius: 10px;
@@ -31,7 +36,6 @@ export default {
 
 .progress-bar-fill {
   height: 100%;
-  background-color: #4caf50;
   border-radius: 10px 0 0 10px;
   transition: width 0.3s ease;
 }
