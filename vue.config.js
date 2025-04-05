57c3e87a1334747fefd2f1f67595b5bb0dd86ffa
Thormes Filgueira Leite Pereira <thormes@gmail.com>
2025-04-05 18:17:00 -0300

    feat: :sparkles: Criação de componente para visualizar imagens svg, podendo alterar propriedades via css
    Criação de componente que abre arquivos svg e utiliza-os como html do elemento, possibilitando modificar propriedades da imagem com css

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.module
+      .rule("svg")
+      .oneOf("raw")
+      .resourceQuery(/raw/)
+      .type("asset/source")
+      .end();
+  },
+});
